fe2df7c9f74e27e558cbe778558e6333dab991a0
caleybennett <cbennet7@conncoll.edu>
2019-11-14 16:27:54 -0500

    Fixed Bugs
    Fixed bugs that were in previous commit

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -8,21 +8,25 @@ let player = 'X'
 const changePlayer = newPlayer => {
   if (newPlayer === 'X') {
     player = 'O'
+    $('#message').html(`player O's move`)
   } else if (newPlayer === 'O') {
     player = 'X'
+    $('#message').html(`player x's move`)
   }
 }
 
 const onClickedSquare = event => {
   event.preventDefault()
+  console.log(player)
   if ($(event.target).html() === ' ') {
     $(event.target).html(player)
-    $('#message').html(`player ${player} move`)
+    // $('#message').html(`player ${player} move`)
     const id = event.target.id
-    changePlayer(player)
+    //  $('#message').html(`player ${player} move`)
     moveToGameBoard(id, player)
+    changePlayer(player)
+    console.log(gamelogic.gameBoard)
     gamelogic.winGame(gamelogic.gameBoard)
-    console.log(player)
   } else {
     $('#message').html(`You already clicked here`)
   }
--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -5,31 +5,47 @@ const events = require('./events')
 const gameBoard = ['', '', '', '', '', '', '', '', '', '']
 
 const winGame = (gameBoard) => {
-  if (gameBoard[] !== ' ') {
-    console.log('keep playing board empty')
-  } else if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  if (gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
     console.log('player wins')
-  } else if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  } else if (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
     console.log('player wins')
-  } else if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  } else if (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
     console.log('player wins')
-  } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  } else if (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
     console.log('player wins')
-  } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  } else if (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
     console.log('player wins')
-  } else if (gameBoard[1] === gameBoard[5] && gameBoard[8] === gameBoard[5]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  } else if (gameBoard[1] === 'X' && gameBoard[8] === 'X' && gameBoard[5] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
     console.log('player wins')
-  } else if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  } else if (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
     console.log('player wins')
-  } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
-    $('#message').html(`Yay!!, ${events.player} won!`)
+  } else if (gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
+    console.log('player wins')
+  } else if (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
+    console.log('player wins')
+  } else if (gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
+    console.log('player wins')
+  } else if (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
+    console.log('player wins')
+  } else if (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
+    console.log('player wins')
+  } else if (gameBoard[1] === 'O' && gameBoard[8] === 'O' && gameBoard[5] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
+    console.log('player wins')
+  } else if (gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
     console.log('player wins')
   } else {
     console.log('keep playing')

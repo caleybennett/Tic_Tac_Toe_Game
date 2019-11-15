025c6d6de692e9dfcdd8ebcb4ffe92701997fa91
caleybennett <cbennet7@conncoll.edu>
2019-11-15 11:35:43 -0500

    Added logic for Game over
    Game logic is now completed.. user can win a game or have a tie and does
    not allow a user to add an X or O to any spaces once the game is over.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -3,41 +3,41 @@
 const gamelogic = require('./gamelogic')
 // const playerinfo = require('./playerinfo')
 
-let player = 'X'
-
-const changePlayer = newPlayer => {
-  if (newPlayer === 'X') {
-    player = 'O'
-    $('#message').html(`player O's move`)
-  } else if (newPlayer === 'O') {
-    player = 'X'
-    $('#message').html(`player x's move`)
-  }
-}
-
-const onClickedSquare = event => {
-  event.preventDefault()
-  console.log(player)
-  if ($(event.target).html() === ' ') {
-    $(event.target).html(player)
-    // $('#message').html(`player ${player} move`)
-    const id = event.target.id
-    //  $('#message').html(`player ${player} move`)
-    moveToGameBoard(id, player)
-    changePlayer(player)
-    console.log(gamelogic.gameBoard)
-    gamelogic.winGame(gamelogic.gameBoard)
-  } else {
-    $('#message').html(`You already clicked here`)
-  }
-}
-
-const moveToGameBoard = (id, player) => {
-  gamelogic.gameBoard.splice(id, 0, player)
-}
+// let player = 'X'
+//
+// const changePlayer = newPlayer => {
+//   if (newPlayer === 'X') {
+//     player = 'O'
+//     $('#message').html(`player O's move`)
+//   } else if (newPlayer === 'O') {
+//     player = 'X'
+//     $('#message').html(`player x's move`)
+//   }
+// }
+//
+// const onClickedSquare = event => {
+//   event.preventDefault()
+//   console.log(player)
+//   if ($(event.target).html() === ' ') {
+//     $(event.target).html(player)
+//     // $('#message').html(`player ${player} move`)
+//     const id = event.target.id
+//     //  $('#message').html(`player ${player} move`)
+//     moveToGameBoard(id, player)
+//     changePlayer(player)
+//     console.log(gamelogic.gameBoard)
+//     gamelogic.winGame(gamelogic.gameBoard)
+//   } else {
+//     $('#message').html(`You already clicked here`)
+//   }
+// }
+//
+// const moveToGameBoard = (id, player) => {
+//   gamelogic.gameBoard.splice(id, 0, player)
+// }
 
 const addHandlers = event => {
-  $('.box').on('click', onClickedSquare)
+  $('.box').on('click', gamelogic.onClickedSquare)
 }
 
 module.exports = {
--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -1,58 +1,139 @@
 'use strict'
 // const htmlDoc = require('./index.html')
-const events = require('./events')
+// const events = require('./events')
 
-const gameBoard = ['', '', '', '', '', '', '', '', '', '']
+let player = 'X'
+
+const changePlayer = newPlayer => {
+  if (newPlayer === 'X') {
+    player = 'O'
+    $('#message').html(`player O's move`)
+  } else if (newPlayer === 'O') {
+    player = 'X'
+    $('#message').html(`player x's move`)
+  }
+}
+const gameOver = function (boolean) {
+  if (boolean === true) {
+    console.log('game over')
+    $('.box').off('click', onClickedSquare)
+  }
+}
+const drawGameOneValue = gameValue => {
+  return gameValue !== ''
+}
+const drawGame = (gameboard) => {
+  if (gameboard.every(drawGameOneValue)) {
+    $('#message').html(`it's tie! please start a new game`)
+  }
+}
+
+const onClickedSquare = event => {
+  event.preventDefault()
+  if ($(event.target).html() === ' ') {
+    $(event.target).html(player)
+    const id = event.target.id
+    moveToGameBoard(id, player)
+    changePlayer(player)
+    console.log(gameBoard)
+    winGame(gameBoard)
+    drawGame(gameBoard)
+  } else {
+    $('#message').html(`You already clicked here`)
+  }
+}
+
+const moveToGameBoard = (id, player) => {
+  gameBoard.splice(id, 1, player)
+}
+
+const gameBoard = ['', '', '', '', '', '', '', '', '']
+
+// const be able to iterate through every method in the array if itdoes not equal
+// empty string
+
+// const drawGameOneValue = gameValue => {
+//   return gameValue !== ''
+// }
+//
+// const drawGame = (gameboard) => {
+//   if (gameboard.every(drawGameOneValue)) {
+//     $('#message').html(`it's tie! please start a new game`)
+//   }
+// }
 
 const winGame = (gameBoard) => {
   if (gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') {
     $('#message').html(`Yay!!, player X won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') {
     $('#message').html(`Yay!!, player X won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') {
     $('#message').html(`Yay!!, player X won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') {
     $('#message').html(`Yay!!, player X won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') {
     $('#message').html(`Yay!!, player X won!`)
-    console.log('player wins')
-  } else if (gameBoard[1] === 'X' && gameBoard[8] === 'X' && gameBoard[5] === 'X') {
+    gameOver(true)
+  } else if (gameBoard[2] === 'X' && gameBoard[8] === 'X' && gameBoard[5] === 'X') {
     $('#message').html(`Yay!!, player X won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') {
     $('#message').html(`Yay!!, player X won!`)
-    console.log('player wins')
+    gameOver(true)
+  } else if (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X') {
+    $('#message').html(`Yay!!, player X won!`)
+    gameOver(true)
   } else if (gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O') {
     $('#message').html(`Yay!!, player O won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O') {
     $('#message').html(`Yay!!, player O won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O') {
     $('#message').html(`Yay!!, player O won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O') {
     $('#message').html(`Yay!!, player O won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O') {
     $('#message').html(`Yay!!, player O won!`)
-    console.log('player wins')
-  } else if (gameBoard[1] === 'O' && gameBoard[8] === 'O' && gameBoard[5] === 'O') {
+    gameOver(true)
+  } else if (gameBoard[2] === 'O' && gameBoard[8] === 'O' && gameBoard[5] === 'O') {
     $('#message').html(`Yay!!, player O won!`)
-    console.log('player wins')
+    gameOver(true)
   } else if (gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O') {
     $('#message').html(`Yay!!, player O won!`)
-    console.log('player wins')
+    gameOver(true)
+  } else if (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O') {
+    $('#message').html(`Yay!!, player O won!`)
+    gameOver(true)
   } else {
     console.log('keep playing')
   }
 }
 
+// for (let i = 0; i < gameBoard.length; i++) {
+//   if (gameBoard[i] === '') {
+//     gameOver(false)
+//     console.log('game not over')
+//   } else if (gameBoard[i] === 'X' || gameBoard[i] === 'O') {
+//     gameOver(true)
+//     console.log('game over')
+//   }
+// }
+
+// game is over when one player wins, or the board is full
+// game over function: if game is over (true) another else statement saying game is over
+// create a function game over
+// game over is true when board is full-- for loop to check.
+
 module.exports = {
   gameBoard,
-  winGame
+  winGame,
+  onClickedSquare,
+  gameOver
 }
